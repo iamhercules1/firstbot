9eb276ed28e4f6245b0d0a69bc327ec91f593de0
unknown <ikhercules@live.com>
2020-04-15 15:35:57 -0400

    "desktop Changes"

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ class Tinderbot ():
     def __init__(self):
         self.driver = webdriver.Chrome()
 
-    def login(self):
+    def login():
         self.driver.get(https://tinder.com/)
 
         sleep(2)
